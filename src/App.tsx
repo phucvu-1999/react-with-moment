5d28f56f22c69525101ff4c86658511763121e3a
Tien Phuc <leesinvn123@gmail.com>
2022-04-03 22:32:25 +0700

    Convert and format date to string

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import moment from "moment";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let m = moment();
+
+  // current Date and time
+
+  // Create from ISO 8601 string
+  m = moment("2022-04-03T23:24:12.000");
+
+  // Using format
+  m = moment("03/04/2022 10:29PM", "DD/MM/YYYY h/mmA");
+  console.log("moment in string: ", m.toString());
+  console.log("moment in ISOS string: ", m.toISOString());
+
   return (
     <div className="App">
       <header className="App-header">
